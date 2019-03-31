1f47f14646471efdb93fec23e4ac4aaac208b7d5
sellabae <devsellabae@gmail.com>
2019-03-30 18:14:37 -0700

    Organized the formatting for PutDec and GetDec

--- lab13.asm
+++ lab13.asm
@@ -1,4 +1,4 @@
-;***************************************
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; "Sum 1 to n Program"
 ; CECS 524 Spring2019 - Lab13
 ;
@@ -8,7 +8,7 @@
 ;
 ; Sella Bae
 ; 3/29/2019
-;***************************************
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 include Pcmac.inc
 .MODEL small
@@ -16,7 +16,7 @@ include Pcmac.inc
 
 ;[Main]-----------------------------------------
 .DATA
-ProgTitle   db  '[Sum from 1 to n]','$'
+ProgTitle   db  '[Sum 1 to n Program]',0Dh,0Ah,'$'  ;string with new line
 PromNumber  db  'Enter a Number: ','$'
 inputNum    dw  ?
 resultSum   dw  ?
@@ -27,8 +27,7 @@ Main    PROC
         MOV AX, @data
         MOV ds, AX
 
-        sPutStr ProgTitle ;print program title
-        _putch  13, 10
+        sPutStr ProgTitle   ;print program title
 
         ;get input number
         sPutStr PromNumber
@@ -37,7 +36,7 @@ Main    PROC
         ;TODO: check if inputNum is an integer number, if not ask again.
 
         ;calculate sum of 1..n calling subprogram RecAdd
-        ; Sum = RecAdd(Num)
+        ;;Sum = RecAdd(Num)
         MOV   AX, inputNum
         PUSH  AX        ;pass argument to RecAdd
         CALL  RecAdd    ;call the subprogram RecAdd
@@ -68,22 +67,22 @@ RecAdd  PROC
         MOV   n, AX
 
         ;calculate the sum
-        ;;; if(n==1)
-        ;;;      return 1;
-        ;;;  else
-        ;;;      return n + recadd(n-1);
+        ;; if(n==1)
+        ;;      return 1;
+        ;;  else
+        ;;      return n + recadd(n-1);
 
         CMP   n, 1          ; IF (n == 1)
         JNE   NumNotOne     ; ELSE goto NumNotOne
         MOV   retSum, 1     ; THEN retSum=1
         JMP    RecAddDone   ;      and goto RecAddDone
 NumNotOne:                  ; ELSE { Sum = RecAdd(Num) }
-        MOV   AX, n       ;pass the argument Num to RecAdd
-        SUB   AX, 1       ; n-1
-        PUSH  AX
-        CALL  RecAdd      ;call the subprogram RecAdd
-        ADD   AX, n       ; ax = RecAdd(n-1) + n
-        MOV   retSum, AX  ;move the result from RecAdd to Sum
+        MOV   AX, n         ;
+        SUB   AX, 1         ; ax = n-1
+        PUSH  AX            ;pass AX as a parameter of subprogram
+        CALL  RecAdd        ;call the subprogram RecAdd
+        ADD   AX, n         ; ax = RecAdd(n-1) + n
+        MOV   retSum, AX    ;move the result from RecAdd to Sum
 
 RecAddDone:
         ;print msg 'RecAdd:n Sum:sum'
@@ -93,7 +92,7 @@ RecAddDone:
         sPutStr PromSubSum  ;print msg 'Sum:'
         MOV   AX, retSum
         Call PutDec         ;print sum
-        _putch 13,10d       ;print newline
+        _putch 13,10       ;print newline
 
         ;return retSum
         MOV   AX, retSum
@@ -113,38 +112,37 @@ PutDec  PROC
         push    bx
         push    cx
         push    dx
-        cmp ax, -32768 ;    -32768 is a special case as there
-        jne TryNeg ;      is no representation of +32768
+        cmp     ax, -32768; -32768 is a special case as there
+        jne     TryNeg    ; is no representation of +32768
         _PutStr M32768
-        jmp Done
+        jmp     Done
 TryNeg:
-        cmp ax, 0 ;     If number is negative ...
-        jge NotNeg
-        mov bx, ax ;      save from it from _PutCh
-        neg bx ;          make it positive and...
-        _PutCh  '-' ;         display a '-' character
-        mov ax, bx ;    To prepare for PushDigs
+        cmp     ax, 0     ; If number is negative ...
+        jge     NotNeg
+        mov     bx, ax    ; save from it from _PutCh
+        neg     bx        ; make it positive and...
+        _PutCh  '-'       ; display a '-' character
+        mov     ax, bx    ; To prepare for PushDigs
 NotNeg:
-        mov cx, 0 ;     Initialize digit count
-        mov bx, 10 ;    Base of displayed number
+        mov     cx, 0     ; Initialize digit count
+        mov     bx, 10    ; Base of displayed number
 PushDigs:
-        sub dx, dx ;    Convert ax to unsigned double-word
-        div bx
-        add dl, '0' ;   Compute the Ascii digit...
-        push    dx ;        ...push it (can push words only)...
-        inc cx ;        ...and count it
-        cmp ax, 0   ;   Don't display leading zeroes
-        jne PushDigs
-    ;
-PopDigs:    ;       Loop to display the digits
-        pop dx ;          (in reverse of the order computed)
+        sub     dx, dx    ; Convert ax to unsigned double-word
+        div     bx
+        add     dl, '0'   ; Compute the Ascii digit...
+        push    dx        ; ...push it (can push words only)...
+        inc     cx        ; ...and count it
+        cmp     ax, 0     ; Don't display leading zeroes
+        jne     PushDigs
+PopDigs:      ; Loop to display the digits
+        pop     dx        ; (in reverse of the order computed)
         _PutCh  dl
         loop    PopDigs
 Done:
-        pop dx ;    Restore registers
-        pop cx
-        pop bx
-        pop ax
+        pop     dx        ; Restore registers
+        pop     cx
+        pop     bx
+        pop     ax
         ret
 PutDec  ENDP
 
@@ -153,44 +151,44 @@ PutDec  ENDP
   Sign    DB  ?
 .CODE
 GetDec  PROC
-        push    bx ;        Don't need to save ax, but bx, cx, ...
-        push    cx ;        ...dx must be saved and restored
+        push    bx        ; Don't need to save ax, but bx, cx, ...
+        push    cx        ; ...dx must be saved and restored
         push    dx
-        mov bx, 0 ;     accumulated NumberValue in bx := 0
-        mov cx, 10
-        mov Sign, '+' ; Guess that sign will be '+'
-        _GetCh  ;       Read character ==> al
-        cmp al, '-' ;   Is first character a minus sign?
-        jne AfterRead
-        mov Sign, '-' ;   yes
+        mov     bx, 0     ; accumulated NumberValue in bx := 0
+        mov     cx, 10
+        mov     Sign, '+' ; Guess that sign will be '+'
+        _GetCh            ; Read character ==> al
+        cmp     al, '-'   ; Is first character a minus sign?
+        jne     AfterRead
+        mov     Sign, '-' ; yes
 ReadLoop:
         _GetCh
 AfterRead:
-        cmp al, '0' ;   Is character a digit?
-        jl  Done2 ;        No
-        cmp al, '9'
-        jg  Done2 ;        No
-        sub al, '0' ;     Yes, cvt to DigitValue and extend to a
-        mov ah, 0 ;        word (so we can add it to NumberValue)
-        xchg    ax, bx ;    Save DigitValue
-            ;          and set up NumberValue for mul
-        mul cx ;        NumberValue * 10 ...
-        add ax, bx ;      + DigitValue ...
-        mov bx, ax ;      ==> NumberValue
-        jmp ReadLoop
-        Done2:
-        cmp al, 13 ;    If last character read was a RETURN...
-        jne NoLF
-        _PutCh 10 ;     ...echo a matching line feed
+        cmp     al, '0'   ; Is character a digit?
+        jl      Done2     ; No
+        cmp     al, '9'
+        jg      Done2     ; No
+        sub     al, '0'   ; Yes, cvt to DigitValue and extend to a
+        mov     ah, 0     ; word (so we can add it to NumberValue)
+        xchg    ax, bx    ; Save DigitValue
+        ; and set up NumberValue for mul
+        mul     cx        ; NumberValue * 10 ...
+        add     ax, bx    ; + DigitValue ...
+        mov     bx, ax    ; ==> NumberValue
+        jmp     ReadLoop
+Done2:
+        cmp     al, 13    ; If last character read was a RETURN...
+        jne     NoLF
+        _PutCh  10        ; ...echo a matching line feed
 NoLF:
-        cmp Sign, '-'
-        jne Positive
-        neg bx ;        Final result is in bx
+        cmp     Sign, '-'
+        jne     Positive
+        neg     bx        ; Final result is in bx
 Positive:
-        mov ax, bx ;    Returned value --> ax
-        pop dx ;        restore registers
-        pop cx
-        pop bx
+        mov     ax, bx    ; Returned value --> ax
+        pop     dx        ; restore registers
+        pop     cx
+        pop     bx
         ret
 GetDec  ENDP
 
